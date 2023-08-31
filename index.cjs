9a636d770ff8418e9861ba0875c3f3d29cc8736b
Jordan Dinis <jtyler9999@gmail.com>
2023-08-30 22:24:54 -0700

    created GET to return all cars at /cars

--- index.cjs
+++ index.cjs
@@ -17,66 +17,64 @@ const corsOptions = {
    optionSuccessStatus: 200,
 }
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 app.use(cors(corsOptions));
 
 // Makes Express parse the JSON body of any requests and adds the body to the req object
 app.use(bodyParser.json());
 
-// app.use(async (req, res, next) => {
-//   try {
-//     // Connecting to our SQL db. req gets modified and is available down the line in other middleware and endpoint functions
-//     req.db = await pool.getConnection();
-//     req.db.connection.config.namedPlaceholders = true;
+app.use(async (req, res, next) => {
+  try {
+    // Connecting to our SQL db. req gets modified and is available down the line in other middleware and endpoint functions
+    req.db = await pool.getConnection();
+    req.db.connection.config.namedPlaceholders = true;
 
-//     // Traditional mode ensures not null is respected for unsupplied fields, ensures valid JavaScript dates, etc.
-//     await req.db.query('SET SESSION sql_mode = "TRADITIONAL"');
-//     await req.db.query(`SET time_zone = '-8:00'`);
+    // Traditional mode ensures not null is respected for unsupplied fields, ensures valid JavaScript dates, etc.
+    await req.db.query('SET SESSION sql_mode = "TRADITIONAL"');
+    await req.db.query(`SET time_zone = '-8:00'`);
 
-//     // Moves the request on down the line to the n ext middleware functions and/or the endpoint it's headed for
-//     await next();
+    // Moves the request on down the line to the n ext middleware functions and/or the endpoint it's headed for
+    await next();
 
-//     // After the endpoint has been reached and resolved, disconnects from the database
-//     req.db.release();
-//   } catch (err) {
-//     // If anything downstream throw an error, we must release the connection allocated for the request
-//     console.log(err)
-//     // If an error occurs, disconnects from the database
-//     if (req.db) req.db.release();
-//     throw err;
-//   }
-// });
+    // After the endpoint has been reached and resolved, disconnects from the database
+    req.db.release();
+  } catch (err) {
+    // If anything downstream throw an error, we must release the connection allocated for the request
+    console.log(err)
+    // If an error occurs, disconnects from the database
+    if (req.db) req.db.release();
+    throw err;
+  }
+});
 
-// Creates a GET endpoint at <WHATEVER_THE_BASE_URL_IS>/students
-app.get('/students', (req, res) => {
-  console.log('GET to /students');
-  const students = [
-    {
-      id: 1,
-      name: 'Ramiro'
-    },
-    {
-      id: 2,
-      name: 'Bryan'
-    },
-    {
-      id: 3,
-      name: 'Kenneth'
-    }
-  ];
+// Creates a GET endpoint at <WHATEVER_THE_BASE_URL_IS>
+app.get('/', (req, res) => {
 
   // Attaches JSON content to the response
-  res.json({ students });
+  res.json('Hi');
+});
+
+// Returns all the cars in the database
+app.get('/cars',async (req,res)=>{
+  try{
+    const query = await req.db.query(
+      `SELECT * FROM car;`
+    )
+    res.json({success: true, message: query[0], data: null})
+  }
+  catch (err){
+    res.json({success: false, message: err, data: null})
+  }
 });
 
 // Creates a POST endpoint at <WHATEVER_THE_BASE_URL_IS>/students
-app.post('/students', (req, res) => {
+app.post('/cars', (req, res) => {
   console.log('POST to /students', req.body);
 
   res.json({ success: true });
